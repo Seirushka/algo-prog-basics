27fc35cb0b7c72251ee8651d2dc61e818fb101d4
Seiru <122806832+Seirushka@users.noreply.github.com>
2024-12-23 23:40:21 +0300

    Add files via upload

---
+++ task16.jl
@@ -0,0 +1,17 @@
+using HorizonSideRobots
+
+
+function shuttle!(stop_condition::Function, robot, side) 
+    side_n, count::Int = Ost, 0
+    while !stop_condition(side) 
+        count += 1
+        move!(robot, side_n, count) 
+        side_n = inverse(side_n) 
+    end 
+end
+
+
+HorizonSideRobots.move!(robot, side, num_steps) = for _ in 1:num_steps move!(robot, side) end
+
+
+inverse(side::HorizonSide) = HorizonSide((Int(side) + 2) % 4)
